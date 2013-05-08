da71e89f2795991365d807fac6534beb1d5454ed
mypapit@gmail.com <mypapit@gmail.com@aabe5cbf-b7d5-0154-e441-fb7da44b8e5e>
2013-05-08 18:04:38 +0000

    change copyright notice to GNU GPL v3

--- src/net/mypapit/mobile/simplediscount/SettingsActivity.java
+++ src/net/mypapit/mobile/simplediscount/SettingsActivity.java
@@ -1,3 +1,26 @@
+/*
+ 	Quick Discount Calculator
+	Copyright 2013 Mohammad Hafiz bin Ismail <mypapit@gmail.com>
+	http://blog.mypapit.net/
+	http://myrepeater.googlecode.com/
+ 
+ This file is part of Quick Discount Calculator.
+
+    Quick Discount Calculator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Quick Discount Calculator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Quick Discount Calculator.  If not, see <http://www.gnu.org/licenses/> 
+ * 
+ */
+
 package net.mypapit.mobile.simplediscount;
 
 import android.app.Activity;
